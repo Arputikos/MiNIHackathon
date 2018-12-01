0346f79421c2e2c2c09be517c9166a4a40d23dc6
bulletsman04 <bulletsman04@gmail.com>
2018-12-01 14:14:04 +0100

    popup - bulletsman scene

---
+++ MiNIHackathon/Assets/Scripts/popupScript.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class popupScript : MonoBehaviour
+{
+
+    public float maxTime = 1;
+    public float time = 0;
+
+    public int speed = 3;
+	// Use this for initialization
+	void Start () {
+	    Init("Test", Color.green);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	   
+        Vector3 pos = this.transform.position + new Vector3(0, speed * 1.0f * Time.deltaTime, 0);
+	    this.transform.position = pos;
+	    time += Time.deltaTime;
+	    Debug.Log(time);
+        if (time >= maxTime)
+	    {
+            DestroyPopup();
+	    }
+	}
+
+     void DestroyPopup()
+    {
+       Destroy(this.gameObject);
+    }
+
+    public void Init(string text, Color color)
+    {
+        var textMesh = gameObject.GetComponentInChildren<TextMesh>();
+        textMesh.text = text;
+        textMesh.color = color;
+    }
+}
